eed370176561ee34e3316137cb9c830483aaaa14
localadmin <eaton.chrisc@gmail.com>
2018-09-06 08:42:00 -0500

    fixed paste problem

--- ansible/makeinv.py
+++ ansible/makeinv.py
@@ -4,15 +4,13 @@ import boto3
 
 regions = ['us-east-1']
 
-#instances = ec2.instances.filter#    Filters=[{'Name': 'instance-state-name', $
-
-running = {}
+#instances = ec2.instances.filter#    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
 for item in regions:
         ec2 = boto3.resource('ec2',region_name=item)
         print("Listing instances in " + item)
-        instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name$
-
+        instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+        
         for instance in instances:
 
            # print("\n")
