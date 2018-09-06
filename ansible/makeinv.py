7c35f1eefbf1191dfc9b3c5c3a734130ea30b478
localadmin <eaton.chrisc@gmail.com>
2018-09-06 08:42:49 -0500

    added back variable

--- ansible/makeinv.py
+++ ansible/makeinv.py
@@ -5,12 +5,12 @@ import boto3
 regions = ['us-east-1']
 
 #instances = ec2.instances.filter#    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-
+running = {}
 for item in regions:
         ec2 = boto3.resource('ec2',region_name=item)
         print("Listing instances in " + item)
         instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-        
+
         for instance in instances:
 
            # print("\n")
