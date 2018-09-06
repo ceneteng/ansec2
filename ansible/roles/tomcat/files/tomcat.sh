9d8c8f188619842207b8859cd1dfbcbc3f4d6cea
localadmin <eaton.chrisc@gmail.com>
2018-09-06 15:32:59 -0500

    fixed bash script issues

--- ansible/roles/tomcat/files/tomcat.sh
+++ ansible/roles/tomcat/files/tomcat.sh
@@ -2,10 +2,10 @@
 mkdir /opt/tomcat
 tar xzvf /tmp/apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
 chgrp -R tomcat /opt/tomcat
-chmod -r g+r /opt/tomcat/conf
+chmod -R g+r /opt/tomcat/conf
 chmod g+x /opt/tomcat/conf
 chown -R tomcat /opt/tomcat/webapps/ /opt/tomcat/work/ /opt/tomcat/temp/ /opt/tomcat/logs/
-update-java-alternatives -logs
+update-java-alternatives -l
 
 systemctl daemon-reload
 systemctl start tomcat
